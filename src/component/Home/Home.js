e1977c0a49c880e7e07f47a5a507a6c1286ed4d4
esimo13 <obak139@gmail.com>
2025-02-07 23:07:35 +0600

    build fixed

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useEffect } from "react";
-import { CgMouse } from "react-icons/all";
+// import { CgMouse } from "react-icons/all";
 import "./Home.css";
 import ProductCard from "./ProductCard.js";
 import MetaData from "../layout/MetaData";
@@ -33,11 +33,11 @@ const Home = () => {
             <h2>Welcome to Ecommerce</h2>
             <h1>FIND AMAZING PRODUCTS BELOW</h1>
 
-            <a href="#container">
+            {/* <a href="#container">
               <button>
                 Scroll <CgMouse />
               </button>
-            </a>
+            </a> */}
           </div>
 
           <h2 className="homeHeading">Featured Products</h2>
