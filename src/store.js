7f04505c2aabb5cb2d508c0e11bbc0f0b0fdf27d
esimo13 <obak139@gmail.com>
2025-02-11 01:04:09 +0600

    implement redux-persist

--- src/store.js
+++ src/store.js
@@ -1,193 +1,6 @@
-// import { createStore, combineReducers, applyMiddleware } from "redux";
-// import thunk from "redux-thunk";
-// import { composeWithDevTools } from "redux-devtools-extension";
-// import {
-//   newProductReducer,
-//   newReviewReducer,
-//   productDetailsReducer,
-//   productReducer,
-//   productReviewsReducer,
-//   productsReducer,
-//   reviewReducer,
-// } from "./reducers/productReducer";
-
-// import {
-//   newAuctionReducer,
-//   auctionDetailsReducer,
-//   auctionReducer,
-//   auctionsReducer,
-// } from "./reducers/auctionReducer";
-
-// import {
-//   allUsersReducer,
-//   forgotPasswordReducer,
-//   profileReducer,
-//   userDetailsReducer,
-//   userReducer,
-// } from "./reducers/userReducer";
-
-// import { cartReducer } from "./reducers/cartReducer";
-// import {
-//   allOrdersReducer,
-//   myOrdersReducer,
-//   newOrderReducer,
-//   orderDetailsReducer,
-//   orderReducer,
-// } from "./reducers/orderReducer";
-
-// const reducer = combineReducers({
-//   auctions: auctionsReducer,
-//   auctionDetails: auctionDetailsReducer,
-//   newAuction: newAuctionReducer,
-//   auction: auctionReducer,
-//   //product
-//   products: productsReducer,
-//   productDetails: productDetailsReducer,
-//   user: userReducer,
-//   profile: profileReducer,
-//   forgotPassword: forgotPasswordReducer,
-//   cart: cartReducer,
-//   newOrder: newOrderReducer,
-//   myOrders: myOrdersReducer,
-//   orderDetails: orderDetailsReducer,
-//   newReview: newReviewReducer,
-//   newProduct: newProductReducer,
-//   product: productReducer,
-//   allOrders: allOrdersReducer,
-//   order: orderReducer,
-//   allUsers: allUsersReducer,
-//   userDetails: userDetailsReducer,
-//   productReviews: productReviewsReducer,
-//   review: reviewReducer,
-// });
-
-// const userFromStorage = localStorage.getItem("token")
-//    { token: localStorage.getItem("token") }
-//   : null;
-
-// let initialState = {
-//   cart: {
-//     cartItems: localStorage.getItem("cartItems")
-//        JSON.parse(localStorage.getItem("cartItems"))
-//       : [],
-//     shippingInfo: localStorage.getItem("shippingInfo")
-//        JSON.parse(localStorage.getItem("shippingInfo"))
-//       : {},
-//   },
-//   user: userFromStorage,
-// };
-
-// const middleware = [thunk];
-
-// const store = createStore(
-//   reducer,
-//   initialState,
-//   composeWithDevTools(applyMiddleware(...middleware))
-// );
-
-// export default store;
-// import { createStore, combineReducers, applyMiddleware } from "redux";
-
-// import thunk from "redux-thunk";
-// import { composeWithDevTools } from "redux-devtools-extension";
-// import {
-//   newProductReducer,
-//   newReviewReducer,
-//   productDetailsReducer,
-//   productReducer,
-//   productReviewsReducer,
-//   productsReducer,
-//   reviewReducer,
-// } from "./reducers/productReducer";
-
-// import {
-//   newAuctionReducer,
-//   auctionDetailsReducer,
-//   auctionReducer,
-//   auctionsReducer,
-// } from "./reducers/auctionReducer";
-
-// import {
-//   allUsersReducer,
-//   forgotPasswordReducer,
-//   profileReducer,
-//   userDetailsReducer,
-//   userReducer,
-// } from "./reducers/userReducer";
-
-// import { cartReducer } from "./reducers/cartReducer";
-// import {
-//   allOrdersReducer,
-//   myOrdersReducer,
-//   newOrderReducer,
-//   orderDetailsReducer,
-//   orderReducer,
-// } from "./reducers/orderReducer";
-
-// // Combine all reducers
-// const reducer = combineReducers({
-//   auctions: auctionsReducer,
-//   auctionDetails: auctionDetailsReducer,
-//   newAuction: newAuctionReducer,
-//   auction: auctionReducer,
-//   products: productsReducer,
-//   productDetails: productDetailsReducer,
-//   user: userReducer,
-//   profile: profileReducer,
-//   forgotPassword: forgotPasswordReducer,
-//   cart: cartReducer,
-//   newOrder: newOrderReducer,
-//   myOrders: myOrdersReducer,
-//   orderDetails: orderDetailsReducer,
-//   newReview: newReviewReducer,
-//   newProduct: newProductReducer,
-//   product: productReducer,
-//   allOrders: allOrdersReducer,
-//   order: orderReducer,
-//   allUsers: allUsersReducer,
-//   userDetails: userDetailsReducer,
-//   productReviews: productReviewsReducer,
-//   review: reviewReducer,
-// });
-
-// // Check if user is in local storage and create user object from that
-// const userFromStorage = localStorage.getItem("token")
-//    {
-//       token: localStorage.getItem("token"),
-//       isAuthenticated: true,
-//       user: {},
-//     }
-//   : {
-//       isAuthenticated: false,
-//       user: null,
-//       token: null,
-//     };
-
-// // Initial state
-// let initialState = {
-//   cart: {
-//     cartItems: localStorage.getItem("cartItems")
-//        JSON.parse(localStorage.getItem("cartItems"))
-//       : [],
-//     shippingInfo: localStorage.getItem("shippingInfo")
-//        JSON.parse(localStorage.getItem("shippingInfo"))
-//       : {},
-//   },
-//   user: userFromStorage, // Initialized user state
-// };
-
-// // Middleware for Redux Thunk
-// const middleware = [thunk];
-
-// // Create the Redux store with combined reducers, initial state, and middleware
-// const store = createStore(
-//   reducer,
-//   initialState,
-//   composeWithDevTools(applyMiddleware(...middleware))
-// );
-
-// export default store;
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage"; // Defaults to localStorage for web
+import { persistStore, persistReducer } from "redux-persist";
 import {
   newProductReducer,
   newReviewReducer,
@@ -221,9 +34,40 @@ import {
   orderDetailsReducer,
   orderReducer,
 } from "./reducers/orderReducer";
+// Persist configuration
+const persistConfig = {
+  key: "root", // Key to store the state in storage
+  storage, // Storage to use (localStorage)
+  whitelist: ["user", "cart"], // Only persist the user and cart state
+};
 
 // Combine all reducers
-const reducer = {
+// const reducer = {
+//   auctions: auctionsReducer,
+//   auctionDetails: auctionDetailsReducer,
+//   newAuction: newAuctionReducer,
+//   auction: auctionReducer,
+//   products: productsReducer,
+//   productDetails: productDetailsReducer,
+//   user: userReducer,
+//   profile: profileReducer,
+//   forgotPassword: forgotPasswordReducer,
+//   cart: cartReducer,
+//   newOrder: newOrderReducer,
+//   myOrders: myOrdersReducer,
+//   orderDetails: orderDetailsReducer,
+//   newReview: newReviewReducer,
+//   newProduct: newProductReducer,
+//   product: productReducer,
+//   allOrders: allOrdersReducer,
+//   order: orderReducer,
+//   allUsers: allUsersReducer,
+//   userDetails: userDetailsReducer,
+//   productReviews: productReviewsReducer,
+//   review: reviewReducer,
+// };
+
+const rootReducer = combineReducers({
   auctions: auctionsReducer,
   auctionDetails: auctionDetailsReducer,
   newAuction: newAuctionReducer,
@@ -246,7 +90,11 @@ const reducer = {
   userDetails: userDetailsReducer,
   productReviews: productReviewsReducer,
   review: reviewReducer,
-};
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+/*
 
 // Check if user is in local storage and create user object from that
 const userFromStorage = localStorage.getItem("token")
@@ -260,7 +108,7 @@ const userFromStorage = localStorage.getItem("token")
       user: null,
       token: null,
     };
-// const token = localStorage.getItem("token");
+
 
 // Initial state
 let initialState = {
@@ -273,7 +121,7 @@ let initialState = {
       : {},
   },
   user: userFromStorage, // Initialized user state
-  // auth: { token },
+  
 };
 
 // Create the Redux store with combined reducers, initial state, and middleware
@@ -281,5 +129,17 @@ const store = configureStore({
   reducer,
   preloadedState: initialState,
 });
+
+export default store; */
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false, // Avoid warnings for non-serializable actions in persist
+    }),
+});
+
+export const persistor = persistStore(store);
 
 export default store;
