eb178bdeb9d541d157f7336b4e81aca6b4106522
esimo13 <obak139@gmail.com>
2025-02-10 19:01:42 +0600

    configure store store js

--- src/store.js
+++ src/store.js
@@ -62,16 +62,16 @@
 // });
 
 // const userFromStorage = localStorage.getItem("token")
-//   ? { token: localStorage.getItem("token") }
+//    { token: localStorage.getItem("token") }
 //   : null;
 
 // let initialState = {
 //   cart: {
 //     cartItems: localStorage.getItem("cartItems")
-//       ? JSON.parse(localStorage.getItem("cartItems"))
+//        JSON.parse(localStorage.getItem("cartItems"))
 //       : [],
 //     shippingInfo: localStorage.getItem("shippingInfo")
-//       ? JSON.parse(localStorage.getItem("shippingInfo"))
+//        JSON.parse(localStorage.getItem("shippingInfo"))
 //       : {},
 //   },
 //   user: userFromStorage,
@@ -86,9 +86,108 @@
 // );
 
 // export default store;
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
+// import { createStore, combineReducers, applyMiddleware } from "redux";
+
+// import thunk from "redux-thunk";
+// import { composeWithDevTools } from "redux-devtools-extension";
+// import {
+//   newProductReducer,
+//   newReviewReducer,
+//   productDetailsReducer,
+//   productReducer,
+//   productReviewsReducer,
+//   productsReducer,
+//   reviewReducer,
+// } from "./reducers/productReducer";
+
+// import {
+//   newAuctionReducer,
+//   auctionDetailsReducer,
+//   auctionReducer,
+//   auctionsReducer,
+// } from "./reducers/auctionReducer";
+
+// import {
+//   allUsersReducer,
+//   forgotPasswordReducer,
+//   profileReducer,
+//   userDetailsReducer,
+//   userReducer,
+// } from "./reducers/userReducer";
+
+// import { cartReducer } from "./reducers/cartReducer";
+// import {
+//   allOrdersReducer,
+//   myOrdersReducer,
+//   newOrderReducer,
+//   orderDetailsReducer,
+//   orderReducer,
+// } from "./reducers/orderReducer";
+
+// // Combine all reducers
+// const reducer = combineReducers({
+//   auctions: auctionsReducer,
+//   auctionDetails: auctionDetailsReducer,
+//   newAuction: newAuctionReducer,
+//   auction: auctionReducer,
+//   products: productsReducer,
+//   productDetails: productDetailsReducer,
+//   user: userReducer,
+//   profile: profileReducer,
+//   forgotPassword: forgotPasswordReducer,
+//   cart: cartReducer,
+//   newOrder: newOrderReducer,
+//   myOrders: myOrdersReducer,
+//   orderDetails: orderDetailsReducer,
+//   newReview: newReviewReducer,
+//   newProduct: newProductReducer,
+//   product: productReducer,
+//   allOrders: allOrdersReducer,
+//   order: orderReducer,
+//   allUsers: allUsersReducer,
+//   userDetails: userDetailsReducer,
+//   productReviews: productReviewsReducer,
+//   review: reviewReducer,
+// });
+
+// // Check if user is in local storage and create user object from that
+// const userFromStorage = localStorage.getItem("token")
+//    {
+//       token: localStorage.getItem("token"),
+//       isAuthenticated: true,
+//       user: {},
+//     }
+//   : {
+//       isAuthenticated: false,
+//       user: null,
+//       token: null,
+//     };
+
+// // Initial state
+// let initialState = {
+//   cart: {
+//     cartItems: localStorage.getItem("cartItems")
+//        JSON.parse(localStorage.getItem("cartItems"))
+//       : [],
+//     shippingInfo: localStorage.getItem("shippingInfo")
+//        JSON.parse(localStorage.getItem("shippingInfo"))
+//       : {},
+//   },
+//   user: userFromStorage, // Initialized user state
+// };
+
+// // Middleware for Redux Thunk
+// const middleware = [thunk];
+
+// // Create the Redux store with combined reducers, initial state, and middleware
+// const store = createStore(
+//   reducer,
+//   initialState,
+//   composeWithDevTools(applyMiddleware(...middleware))
+// );
+
+// export default store;
+import { configureStore } from "@reduxjs/toolkit";
 import {
   newProductReducer,
   newReviewReducer,
@@ -124,7 +223,7 @@ import {
 } from "./reducers/orderReducer";
 
 // Combine all reducers
-const reducer = combineReducers({
+const reducer = {
   auctions: auctionsReducer,
   auctionDetails: auctionDetailsReducer,
   newAuction: newAuctionReducer,
@@ -147,7 +246,7 @@ const reducer = combineReducers({
   userDetails: userDetailsReducer,
   productReviews: productReviewsReducer,
   review: reviewReducer,
-});
+};
 
 // Check if user is in local storage and create user object from that
 const userFromStorage = localStorage.getItem("token")
@@ -175,14 +274,10 @@ let initialState = {
   user: userFromStorage, // Initialized user state
 };
 
-// Middleware for Redux Thunk
-const middleware = [thunk];
-
 // Create the Redux store with combined reducers, initial state, and middleware
-const store = createStore(
+const store = configureStore({
   reducer,
-  initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
-);
+  preloadedState: initialState,
+});
 
 export default store;
